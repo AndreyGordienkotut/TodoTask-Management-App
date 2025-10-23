30f2772e6a02efe5371e55c13a96930c8ca7f8d4
AndreyGordienkotut <andreykagordienko14@gmail.com>
2025-10-23 20:13:19 +0300

    Adding a docker and configuring startup through it. Also adding database creation startup.

--- notificationService/src/test/java/com/notificationService/NotificationServiceApplicationTests.java
+++ notificationService/src/test/java/com/notificationService/NotificationServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.notificationService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NotificationServiceApplicationTests {
 
 	@Test
