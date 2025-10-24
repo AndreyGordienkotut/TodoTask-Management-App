b69ad63e9987e981f53813737f9c0d3843186c65
AndreyGordienkotut <andreykagordienko14@gmail.com>
2025-10-24 22:44:23 +0300

    Adding a docker and configuring startup through it. Also adding database creation startup.

--- taskService/src/test/java/com/taskService/TaskServiceApplicationTests.java
+++ taskService/src/test/java/com/taskService/TaskServiceApplicationTests.java
@@ -1,9 +1,21 @@
 package com.taskService;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+//@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@EnableAutoConfiguration(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class,
+		KafkaAutoConfiguration.class
+})
+@ActiveProfiles("test")
 class TaskServiceApplicationTests {
 	@Test
 	void contextLoads() {
