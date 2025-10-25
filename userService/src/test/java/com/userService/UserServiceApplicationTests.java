d450e1433f4447158b7f0122d8f2e075526abdfa
AndreyGordienkotut <andreykagordienko14@gmail.com>
2025-10-25 14:39:36 +0300

    Adding a docker and configuring startup through it. Also adding database creation startup.

--- userService/src/test/java/com/userService/UserServiceApplicationTests.java
+++ userService/src/test/java/com/userService/UserServiceApplicationTests.java
@@ -3,6 +3,7 @@ package com.userService;
 import com.userService.repository.EmailVerificationTokensRepository;
 import com.userService.repository.RefreshTokenRepository;
 import com.userService.repository.UserRepository;
+import com.userService.service.JwtService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -12,7 +13,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE,
+properties = {
+		"application.security.jwt.secret-key=test-secret",
+		"application.security.jwt.expiration=3600000",
+		"application.security.jwt.refresh-token.expiration=604800000"
+		})
 @EnableAutoConfiguration(exclude = {
 		DataSourceAutoConfiguration.class,
 		HibernateJpaAutoConfiguration.class,
@@ -25,7 +31,8 @@ class UserServiceApplicationTests {
 
 	@MockBean
 	private RefreshTokenRepository refreshTokenRepository;
-
+	@MockBean
+	private JwtService jwtService;
 	@MockBean
 	private EmailVerificationTokensRepository emailVerificationTokensRepository;
 	@Test
